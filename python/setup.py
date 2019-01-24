4b3678d75a3c071807d3079bb78d05189d2fc275
Manas Rachh <manasrachh@gmail.com>
2019-01-24 12:09:45 -0500

    updating setup.py and adding numfourphsy.f

--- python/setup.py
+++ python/setup.py
@@ -12,10 +12,23 @@ list1=['h3dcommon.f','helmrouts3d.f','hfmm3dpartwrap_vec.f','quadread.f',
 list2=['besseljs3d.f','d3hplratree.f','dlaran.f','hkrand.f','prini.f','rotgen.f','rotviarecur.f',
 'cdjseval3d.f','dfft.f','fmmcommon.f','legeexps.f','rotgen2.f','rotproj.f','yrecursion.f']
 
+c_opts = ['c','d','cd']
+st_opts = ['stos','stot','stost']
+p_opts = ['p','g']
+
+list_int = []
+list_int_vec = []
+
+for st in st_opts:
+    for cd in c_opts:
+        for pg in p_opts:
+            list_int.append('hfmm3dpart'+st+cd+pg)
+            list_int_vec.append('hfmm3dpart'+st+cd+pg+'_vec')
+
 ext1 = Extension(
     name='fmm3d_fortran',
     sources=['src/Helmholtz/'+item for item in list1]+['src/Common/'+item for item in list2],
-    f2py_options=['only:','hfmm3dpartwrap.f',':']
+    f2py_options=['only:']+list_int+list_int_vec+[':']
 )
 
 ## TODO: fill in the info below
