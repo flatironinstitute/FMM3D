28e6dabf3790147259d6e0b7eb5adc4340e9834d
manasrachh <manasrachh@gmail.com>
2019-05-17 17:06:48 -0400

    Updating license for dlaran

--- src/Common/dlaran.f
+++ src/Common/dlaran.f
@@ -1,4 +1,53 @@
-      DOUBLE PRECISION FUNCTION DLARAN( ISEED )
+C  Copyright (c) 1992-2013 The University of Tennessee and The University
+C                          of Tennessee Research Foundation.  All rights
+C                          reserved.
+C  Copyright (c) 2000-2013 The University of California Berkeley. All
+C                          rights reserved.
+C  Copyright (c) 2006-2013 The University of Colorado Denver.  All rights
+C                          reserved.
+C  
+C  $COPYRIGHT$
+C  
+C  Additional copyrights may follow
+C  
+C  $HEADER$
+C  
+C  Redistribution and use in source and binary forms, with or without
+C  modification, are permitted provided that the following conditions are
+C  met:
+C  
+C  - Redistributions of source code must retain the above copyright
+C    notice, this list of conditions and the following disclaimer.
+C  
+C  - Redistributions in binary form must reproduce the above copyright
+C    notice, this list of conditions and the following disclaimer listed
+C    in this license in the documentation and/or other materials
+C    provided with the distribution.
+C  
+C  - Neither the name of the copyright holders nor the names of its
+C    contributors may be used to endorse or promote products derived from
+C    this software without specific prior written permission.
+C  
+C  The copyright holders provide no reassurances that the source code
+C  provided does not infringe any patent, copyright, or any other
+C  intellectual property rights of third parties.  The copyright holders
+C  disclaim any liability to any recipient for claims brought against
+C  recipient by any third party for infringement of that parties
+C  intellectual property rights.
+C  
+C  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+C  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+C  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+C  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+C  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+C  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+C  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+C  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+C  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+C  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+C  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+C
+       DOUBLE PRECISION FUNCTION DLARAN( ISEED )
 *
 *  -- LAPACK auxiliary routine (version 3.0) --
 *     Univ. of Tennessee, Univ. of California Berkeley, NAG Ltd.,
