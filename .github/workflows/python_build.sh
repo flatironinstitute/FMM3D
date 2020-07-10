dc01813a40c874da3786751a4cc1895b3e1f8150
Guilherme B Saturnino <guilhermebs@drcmr.dk>
2020-07-10 16:31:55 +0200

    added MacOS CI

--- .github/workflows/python_build.sh
+++ .github/workflows/python_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 # This is an auxiliary script to build wheels
 PYTHON=$1
-echo "PYTHON=$PYTHON" > make.inc 
+echo "PYTHON=$PYTHON" >> make.inc 
 $PYTHON -m pip install numpy
 make python-dist
