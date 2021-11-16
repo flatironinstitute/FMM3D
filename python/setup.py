5e6387f57844288074ec7ee2bba4fa51459ff559
lu1and10 <llu@flatironinstitute.org>
2021-11-16 12:40:49 -0500

    bump python wheel version number

--- python/setup.py
+++ python/setup.py
@@ -83,7 +83,7 @@ ext_st = Extension(
 setup(
     name=pkg_name,
     python_requires='>=3.0.0',
-    version="0.0.5",
+    version="1.0.0",
     author="Zydrunas Gimbutas, Leslie Greengard, Libin Lu, Jeremy Magland, Dhairya Malhotra, Michael O'Neil, Manas Rachh, and Vladimir Rokhlin",
     author_email="mrachh@flatironinstitute.org",
     description="This pacakge contains basic routines for Laplace and Helmholtz fast multipole methods in three dimensions",
