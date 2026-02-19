11f386969ce905cb9cf57525cc02b94531481242
Manas Rachh <mrachh@iitb.ac.in>
2026-02-19 14:45:44 +0530

    fixing some ndiv issues

--- src/Helmholtz/hndiv.f
+++ src/Helmholtz/hndiv.f
@@ -1,4 +1,4 @@
-      subroutine hndiv(eps,ns,nt,ifcharge,ifdipole,ifpgh,
+      subroutine hndiv(dbsize,eps,ns,nt,ifcharge,ifdipole,ifpgh,
      1   ifpghtarg,ndiv,idivflag)
 c
 c
@@ -9,31 +9,35 @@ c       TODO: better way to choose ndiv based on boxsize
 c
 c
       implicit none
-      real *8 eps
+      real *8 eps,dbsize,rfac
       integer *8 ns,nt,ifcharge,ifdipole,ifpgh,ifpghtarg,ndiv
       integer *8 idivflag
 
       idivflag = 0
+      rfac = 1.0d0
+      if (dbsize.gt.8) rfac = (dbsize/8)**1.5d0
 
        if(eps.ge.0.5d-0) then
-         ndiv = 40
+         ndiv = ceiling(40*rfac)
        else if(eps.ge.0.5d-1) then
-         ndiv = 40
+         ndiv = ceiling(40*rfac)
        else if(eps.ge.0.5d-2) then
-         ndiv = 40
+         ndiv = ceiling(40*rfac)
        else if(eps.ge.0.5d-3) then
-         ndiv = 80
+         ndiv = ceiling(120*rfac)
        else if(eps.ge.0.5d-6) then
-         ndiv = 200
+         ndiv = ceiling(300*rfac)
        else if(eps.ge.0.5d-9) then
-         ndiv = 400
+         ndiv = ceiling(600*rfac)
        else if(eps.ge.0.5d-12) then
-         ndiv = 600
+         ndiv = ceiling(900*rfac)
        else if(eps.ge.0.5d-15) then
-         ndiv = 700
+         ndiv = ceiling(1050*rfac)
        else
          ndiv = ns+nt
        endif
+
+       ndiv = min(ndiv,10000)
 
       return
       end
--- src/Helmholtz/hndiv_fast.f
+++ src/Helmholtz/hndiv_fast.f
@@ -1,4 +1,4 @@
-      subroutine hndiv(eps,ns,nt,ifcharge,ifdipole,ifpgh,
+      subroutine hndiv(dbsize,eps,ns,nt,ifcharge,ifdipole,ifpgh,
      1   ifpghtarg,ndiv,idivflag)
 c
 c
@@ -7,31 +7,36 @@ c       based on geometry parameters
 c       
 c
       implicit none
-      real *8 eps
+      real *8 eps,dbsize,rfac
       integer *8 ns,nt,ifcharge,ifdipole,ifpgh,ifpghtarg,ndiv
       integer *8 idivflag
 
       idivflag = 0
+      rfac = 1.0d0
+      if (dbsize.gt.8) rfac = (dbsize/8)**1.5d0
+      
 
        if(eps.ge.0.5d-0) then
-         ndiv = 300
+         ndiv = ceiling(300*rfac)
        else if(eps.ge.0.5d-1) then
-         ndiv = 300
+         ndiv = ceiling(300*rfac)
        else if(eps.ge.0.5d-2) then
-         ndiv = 300
+         ndiv = ceiling(300*rfac)
        else if(eps.ge.0.5d-3) then
-         ndiv = 300
+         ndiv = ceiling(300*rfac)
        else if(eps.ge.0.5d-6) then
-         ndiv = 1000
+         ndiv = ceiling(1000*rfac)
        else if(eps.ge.0.5d-9) then
-         ndiv = 1000
+         ndiv = ceiling(1000*rfac)
        else if(eps.ge.0.5d-12) then
-         ndiv = 1000
+         ndiv = ceiling(1000*rfac)
        else if(eps.ge.0.5d-15) then
-         ndiv = 1000
+         ndiv = ceiling(1000*rfac)
        else
          ndiv = ns+nt
        endif
+
+       ndiv = min(ndiv, 10000)
 
       return
       end
