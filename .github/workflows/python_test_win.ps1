e10f0847ac6554318bc8f6dce7fb44853ac127ef
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2026-03-14 02:46:46 -0400

    Update CI/CD

--- .github/workflows/python_test_win.ps1
+++
@@ -1,6 +0,0 @@
-python -m pip install pytest
-if (-not $?) {throw "Failed to pip install pytest"}
-python -m pip install fmm3dpy -f .\wheelhouse\
-if (-not $?) {throw "Failed to pip install fmm3dpy"}
-python -m pytest -s python/test
-if (-not $?) {throw "Tests failed"}
